6ef652143aceb1a6f4e2118068ddffe89e279bd2
sixtusdev <sixtusushahemba@gmail.com>
2023-02-11 10:29:07 +0100

    commit on index file and js files for download btn

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ function redirectURLIndex() {
 var modal = document.getElementById('myModal')
 
 // Get the button that opens the modal
-var btn = document.getElementById('myBtn')
+var btn = document.getElementById('download')
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName('close')[0]
