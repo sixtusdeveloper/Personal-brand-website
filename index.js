60ef8cc3b53179ff96d90b60058cc9fd76bb73b3
sixtusdev <sixtusushahemba@gmail.com>
2023-02-11 09:10:12 +0100

    commit on all files and updates

---
+++ index.js
@@ -0,0 +1,36 @@
+// let navbar = document.querySelector('header')
+const stickyNavbar = document.querySelector('#header')
+const navbarContent = document.querySelector('.right-items-mobile')
+
+window.onscroll = () => {
+  if (window.scrollY > 50) {
+    stickyNavbar.style.backgroundColor = '#fff'
+    if (navbarContent.classList.contains('active')) {
+      navbarContent.style.display = 'none'
+    } else if (contents.classList.contains('content_active')) {
+      contents.classList.remove('content_active')
+    }
+  } else {
+    stickyNavbar.style.backgroundColor = 'aliceblue'
+    navbarContent.style.display = 'initial'
+    contents.classList.add('content_active')
+  }
+}
+
+// Toggle menu
+// const linkItems = document.qquerySelectorAll('nav .right-items a')
+let toggleBar = document.querySelector('#toggleBar')
+let menuItems = document.querySelector('#navBar')
+// Brands items script
+let toggleContentsBtn = document.querySelector('#toggleContents')
+const contents = document.querySelector('#flexBranditems')
+
+function toggleItems() {
+  toggleBar.classList.toggle('fa-times')
+  menuItems.classList.toggle('active')
+}
+
+function toggleContents() {
+  toggleContentsBtn.classList.toggle('fa-chevron-right')
+  contents.classList.toggle('content_active')
+}
